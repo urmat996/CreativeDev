2055b5cecfae813c68b9e65acc726ecc622396cd
urmat996 <71040516+urmat996@users.noreply.github.com>
2020-10-28 20:27:37 +0600

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f997ef5b40b56001e9528f6",
+				"5f997ef5b40b56001e9528f8"
+			]
+		},
+		"5f997ef5b40b56001e9528f6": {
+			"id": "5f997ef5b40b56001e9528f6",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f997ef5b40b56001e9528f8": {
+			"id": "5f997ef5b40b56001e9528f8",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
