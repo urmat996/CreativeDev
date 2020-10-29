07de8129be2514ab2ee36a450dd2e496cff60a87
urmat996 <71040516+urmat996@users.noreply.github.com>
2020-10-29 17:13:29 +0600

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Image, Text, Icon, Box } from "@quarkly/widgets";
+import { Theme, Image, Text, Icon, Box, Link, Input, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Override, Menu, Section, StackItem, Stack, SocialMedia } from "@quarkly/components";
-import { MdArrowDownward } from "react-icons/md";
+import { Override, Menu, Section, StackItem, Stack, SocialMedia, GoogleMap, Formspree } from "@quarkly/components";
+import { MdArrowDownward, MdLocationOn, MdEmail, MdPhone } from "react-icons/md";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -19,11 +20,11 @@ export default (() => {
 				height="40.799993896484374px"
 				src="https://uploads.quarkly.io/5f9a9825021c7d001f69b460/images/dev.svg?v=2020-10-29T11:03:12.214Z"
 				display="inline-block"
-				position="absolute"
-				bottom="2331.6000061035156px"
-				left="61px"
-				right="1005.4000122070313px"
-				top="30px"
+				position="inherit"
+				bottom="auto"
+				left="95px"
+				right="auto"
+				top="32px"
 			/>
 			<Menu
 				display="flex"
@@ -182,6 +183,180 @@ export default (() => {
 				/>
 			</SocialMedia>
 		</Section>
-		<Image width="64px" height="64px" />
+		<Section
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1547619292-240402b5ae5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80)"
+		>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="32px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdLocationOn}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Visit us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							4998 Hanover Street, New York, 10011
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="64px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdEmail}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Email us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+								hello@company.com
+							</Link>
+						</Text>
+					</Box>
+					<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdPhone}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Call us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							+1 (234) 567-89-00
+							<br />
+							+1 (234) 567-89-00
+						</Text>
+					</Box>
+					<GoogleMap />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="0"
+						margin="48px 0"
+						max-width="360px"
+						position="relative"
+						display="flex"
+					>
+						<Icon
+							category="fa"
+							icon={FaFacebookF}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaTwitter}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+						<Icon
+							category="fa"
+							icon={FaLinkedinIn}
+							width="48px"
+							height="48px"
+							size="24px"
+							margin-right="16px"
+							background="--color-primary"
+							border-radius="50%"
+						/>
+					</Box>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-primary"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree>
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Stack>
+			{"    "}{"    "}
+			<StackItem width="50%" display="flex">
+				{"        "}
+				<Text font="--lead" margin="0px 0px 0px 0px" display="inline-block">
+					{"            "}Second Item{"\n        "}
+				</Text>
+				{"    "}
+			</StackItem>
+		</Stack>
+		<StackItem>
+			<Text font="--lead">
+				Stack item
+			</Text>
+		</StackItem>
 	</Theme>;
 });
