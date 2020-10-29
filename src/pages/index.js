796aa3d0b5fd1f84659af0d59e43d494b0e8a7fc
urmat996 <71040516+urmat996@users.noreply.github.com>
2020-10-29 16:51:37 +0600

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Text } from "@quarkly/widgets";
+import { Theme, Text, Icon, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Override, Menu, Section } from "@quarkly/components";
+import { Override, Menu, Section, StackItem, Stack, SocialMedia } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -28,6 +29,150 @@ export default (() => {
 				<Override slot="link-active" color="--primary" />
 				<Override slot="item" padding="6px" />
 			</Menu>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Striving for perfection in everything we do. Unparalleled service for everyone.
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section
+			text-align="center"
+			background-color="--primary"
+			color="--light"
+			padding="100px 0"
+			sm-padding="40px 0"
+		>
+			<Text
+				as="h5"
+				font="--lead"
+				margin="10px 0 0 0"
+				text-transform="uppercase"
+				letter-spacing="5px"
+			>
+				Second to none
+			</Text>
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0 0 0">
+				About Us
+			</Text>
+			<Text as="p" font="--lead" margin="40px 0 20px 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
+			</Text>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Text
+				as="h1"
+				font="--headline1"
+				md-font="--headline2"
+				max-width="520px"
+				margin="0 auto 5px auto"
+				text-align="center"
+			>
+				We are cute
+			</Text>
+			<Text
+				as="p"
+				color="--grey"
+				max-width="520px"
+				margin="0 auto"
+				text-align="center"
+				font="--lead"
+			>
+				The smartest people work every day to provide the best service and to make our clients happy
+			</Text>
+			<Stack margin-top="40px">
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						CEO
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Nathan K. Joe
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Developer
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						David R. Lema
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Marketing
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Janet E. Morey
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat" />
+					<Text color="--grey" margin="0">
+						Designer
+						<br />
+					</Text>
+					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
+						Claire R. Peery
+						<br />
+					</Text>
+					<Text as="p" margin="20px 0 5px 0">
+						This space is 100% editable. Use it to introduce a team member, describe their work experience and role within the company. This is also a great place to highlight a team member's strong sides.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="60px 0" sm-padding="40px 0">
+			<SocialMedia
+				facebook="https://www.facebook.com/quarklyapp/"
+				twitter="https://twitter.com/quarklyapp"
+				youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+				vkontakte="https://vk.com/quarklyapp"
+				telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+			>
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--light"
+					margin="0 8px"
+					background="--color-grey"
+					hover-background="--color-greyD1"
+				/>
+			</SocialMedia>
 		</Section>
 	</Theme>;
 });
